60aa8882091381f9db4459eb9d3e9e57e841eb0d
saryprinx <sahararamirezesp@gmail.com>
2026-09-21 07:38:23 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ProyectoFinal/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
